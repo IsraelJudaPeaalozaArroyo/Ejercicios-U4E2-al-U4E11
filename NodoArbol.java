cdacf66f0127788de9d349d93401c5627b3f11e0
Israel Juda Peñaloza Arroyo <71736208+IsraelJudaPeaalozaArroyo@users.noreply.github.com>
2020-12-15 16:15:20 -0600

    Create NodoArbol.java

---
+++ NodoArbol.java
@@ -0,0 +1,20 @@
+public class NodoArbol {
+    int info;
+    String nom;
+    Object izq;
+    Object der;
+     class Nodo
+      {
+        int info;
+        String nom;
+        Nodo izq, der;
+        
+      }
+      Nodo raiz;
+      int cant;
+      int altura;
+      
+      public NodoArbol() {
+          raiz=null;
+      }
+}
